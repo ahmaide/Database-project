4743a3cec031bc35a8a7ba8d1774ba41b4fa8b95
ahmaide <87603637+ahmaide@users.noreply.github.com>
2022-05-29 18:32:43 +0300

    Arranged_Order

---
+++ src/main/java/company/database_project/Arranged_Order.java
@@ -0,0 +1,51 @@
+package company.database_project;
+
+public class Arranged_Order extends Order {
+
+    String machine_id;
+    String delivery_date;
+    boolean passed;
+    boolean checked;
+
+    public Arranged_Order(int order_id, String order_date, String machine_type, String pay_method, double discount, int customer_id,
+           int worker_id, String machine_id, String delivery_date, boolean passed, boolean checked) {
+
+        super(order_id, order_date, machine_type, pay_method, discount, customer_id, worker_id);
+        this.machine_id = machine_id;
+        this.delivery_date = delivery_date;
+        this.passed = passed;
+        this.checked = checked;
+    }
+
+    public String getMachine_id() {
+        return machine_id;
+    }
+
+    public void setMachine_id(String machine_id) {
+        this.machine_id = machine_id;
+    }
+
+    public String getDelivery_date() {
+        return delivery_date;
+    }
+
+    public void setDelivery_date(String delivery_date) {
+        this.delivery_date = delivery_date;
+    }
+
+    public boolean isPassed() {
+        return passed;
+    }
+
+    public void setPassed(boolean passed) {
+        this.passed = passed;
+    }
+
+    public boolean isChecked() {
+        return checked;
+    }
+
+    public void setChecked(boolean checked) {
+        this.checked = checked;
+    }
+}
