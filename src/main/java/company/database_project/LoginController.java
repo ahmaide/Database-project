312dea776ac64363bc72396eb5f454b28378ba23
ahmaide <87603637+ahmaide@users.noreply.github.com>
2022-06-03 15:08:08 +0300

    settings page

--- src/main/java/company/database_project/LoginController.java
+++ src/main/java/company/database_project/LoginController.java
@@ -2,7 +2,11 @@ package company.database_project;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
@@ -10,6 +14,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -46,7 +51,7 @@ public class LoginController implements Initializable {
         stage.close();
     }
 
-    public void loginTry(ActionEvent e){
+    public void loginTry(ActionEvent e) throws IOException {
         if(username_box.getText() == ""){
             error_text.setText("Username is empty!");
         }
@@ -57,8 +62,12 @@ public class LoginController implements Initializable {
             else{
                 if(Users.list.containsKey(username_box.getText())){
                     if(Users.list.get(username_box.getText()).getPassword().equals(password_box.getText()) ){
-                        System.out.println("It is Trueeeeeeee!");
                         Users.currentUser = Users.list.get(username_box.getText());
+                        Parent root = FXMLLoader.load(getClass().getResource("settings.fxml"));
+                        stage = (Stage) ((Node)e.getSource()).getScene().getWindow();
+                        Scene scene = new Scene(root);
+                        stage.setScene(scene);
+                        stage.show();
                     }
                     else {
                         error_text.setText("Wrong password");
---
+++ src/main/java/company/database_project/SettingsController.java
@@ -0,0 +1,98 @@
+package company.database_project;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class SettingsController implements Initializable {
+    @FXML
+    private AnchorPane pane;
+
+    @FXML
+    private Button back;
+
+    @FXML
+    private Button deactive;
+
+    @FXML
+    private Button newAccount;
+
+    @FXML
+    private Button change_password;
+
+    @FXML
+    private Button back1;
+
+    @FXML
+    private VBox deactive_box;
+
+    @FXML
+    private PasswordField password_deactive;
+
+    @FXML
+    private Label error1;
+
+    @FXML
+    private Button deactive3;
+
+    @FXML
+    private VBox new_account_box;
+
+    @FXML
+    private PasswordField password_box1;
+
+    @FXML
+    private TextField new_username;
+
+    @FXML
+    private PasswordField new_password;
+
+    @FXML
+    private Label error2;
+
+    @FXML
+    private Button addAcocount;
+
+    @FXML
+    private VBox change_password_box;
+
+    @FXML
+    private PasswordField currentPassword_fornew;
+
+    @FXML
+    private PasswordField newPassword2;
+
+    @FXML
+    private PasswordField reenterNewPassword;
+
+    @FXML
+    private Label error3;
+
+    @FXML
+    private Button changePassword;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        for(Node n : deactive_box.getChildren()){
+            n.setManaged(false);
+        }
+        deactive_box.setManaged(false);
+        for(Node n : new_account_box.getChildren()){
+            n.setManaged(false);
+        }
+        new_account_box.setManaged(false);
+        for(Node n : change_password_box.getChildren()){
+            n.setManaged(false);
+        }
+        change_password_box.setManaged(false);
+    }
+}
