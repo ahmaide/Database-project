58515e1d05935af85abf034f4fd9d128528b1848
ahmaide <87603637+ahmaide@users.noreply.github.com>
2022-06-08 14:10:05 +0300

    sql_coneection2&3

---
+++ src/main/java/company/database_project/SQL_connection2.java
@@ -0,0 +1,42 @@
+package company.database_project;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
+public class SQL_connection2 {
+    private static String dbURL;
+    private static String dbUsername = "root"; // database username
+    private static String dbPassword = "root1234"; // database password
+    private static String URL = "127.0.0.1"; // server location
+    private static String port = "3306"; // port that mysql uses
+    private static String dbName = "final_project"; //database on mysql to connect to
+    private static Connection con;
+
+    public static void ExecuteStatement(String SQL) throws SQLException {
+
+        try {
+            Statement stmt = con.createStatement();
+            stmt.executeUpdate(SQL);
+            stmt.close();
+        } catch (SQLException s) {
+            s.printStackTrace();
+            System.out.println("SQL statement is not executed!");
+
+        }
+    }
+
+    public static void connectDB() throws ClassNotFoundException, SQLException {
+        dbURL = "jdbc:mysql://" + URL + ":" + port + "/" + dbName + "?verifyServerCertificate=false";
+        Properties p = new Properties();
+        p.setProperty("user", dbUsername);
+        p.setProperty("password", dbPassword);
+        p.setProperty("useSSL", "false");
+        p.setProperty("autoReconnect", "true");
+        Class.forName("com.mysql.jdbc.Driver");
+        con = DriverManager.getConnection(dbURL, p);
+    }
+
+}
