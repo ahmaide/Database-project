b993b61d5ec96169ffceb75159411eeeb8b15b67
ahmaide <87603637+ahmaide@users.noreply.github.com>
2022-06-10 21:26:16 +0300

    counting

--- src/main/java/company/database_project/Driver.java
+++ src/main/java/company/database_project/Driver.java
@@ -1,11 +1,15 @@
 package company.database_project;
 
+import java.util.ArrayList;
 import java.util.Map;
 
 public class Driver extends Employee{
 
     public static Map<Integer, Driver> active;
     public static Map<Integer, Driver> list;
+    public static Driver current;
+    public static ArrayList<String> notActive;
+    public static int m = 0;
 
     public Driver(int worker_id, String worker_name, int worker_actualID, int worker_phone, boolean activity, String start_date, String leave_date) {
         super(worker_id, worker_name, worker_actualID, worker_phone, activity, start_date, leave_date);
--- src/main/java/company/database_project/Seller.java
+++ src/main/java/company/database_project/Seller.java
@@ -1,11 +1,15 @@
 package company.database_project;
 
+import java.util.ArrayList;
 import java.util.Map;
 
 public class Seller extends Employee {
 
     public static Map<Integer, Seller> active;
     public static Map<Integer, Seller> list;
+    public static Seller current;
+    public static ArrayList<String> notActive;
+    public static int m = 0;
 
     public Seller(int worker_id, String worker_name, int worker_actualID, int worker_phone, boolean activity, String start_date, String leave_date) {
         super(worker_id, worker_name, worker_actualID, worker_phone, activity, start_date, leave_date);
